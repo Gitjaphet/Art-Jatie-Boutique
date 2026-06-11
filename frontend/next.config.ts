69521e35d49786499009fae14a63109bba13288f
Japhet <japhetvaleureux@gmail.com>
2026-06-11 15:31:53 +0300

    autorise test.com

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -20,6 +20,16 @@ const nextConfig: NextConfig = {
         hostname: "gmoezlcqbrfcutyxpxjw.supabase.co",
         pathname: "/storage/v1/object/public/**",
       },
+      {
+        protocol: "https",
+        hostname: "api.artjatie.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "test.com",
+        pathname: "/**",
+      },
     ],
   },
 };
