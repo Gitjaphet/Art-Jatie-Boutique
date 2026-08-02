26108daf757b9570d3f264edfa6c1343b69afd41
Japhet <japhetvaleureux@gmail.com>
2026-08-02 22:18:52 +0300

    annuler le span

--- frontend/app/formation/FormationPage.tsx
+++ frontend/app/formation/FormationPage.tsx
@@ -434,7 +434,7 @@ export default function FormationPage() {
           </div>
 
           <div className={styles.mediaCard}>
-            <span className={styles.mediaLabel}>Résultats</span>
+            
             <ResultsCarousel images={resultats} />
           </div>
         </div>
